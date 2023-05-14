9857e9ecd4f57237413096afba5b977381f95f17
Diego Herrera <dagomessis@gmail.com>
2023-05-14 15:08:24 -0400

    jaden casing strings

--- JadenCasingStrings.js
+++ JadenCasingStrings.js
@@ -7,6 +7,5 @@
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
-String.prototype.toJadenCase = function () {
-  return this.split(' ').map((e) => e.charAt(0).toUpperCase() + e.slice(1)).join(" ");
-};
+String.prototype.toJadenCase = () => this.split(' ').map((e) => e.charAt(0).toUpperCase() + e.slice(1)).join(" ");
+
