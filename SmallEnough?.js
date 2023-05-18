cc57b94676c12ee636635f77df3568193ebfda30
Diego Herrera <dagomessis@gmail.com>
2023-05-18 10:01:18 -0400

    small enough?

---
+++ SmallEnough?.js
@@ -0,0 +1,15 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+function smallEnough(a, limit){
+    let arr = []
+     for(i of a){
+       if(i <= limit){
+         arr.push(i)
+       } else {
+         return false;
+       }
+     }
+      return arr.length === a.length ? true : false
+   }
