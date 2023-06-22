e2ef8793e15ca6ab5b00be3260664e7b21ff99d5
Diego Herrera <dagomessis@gmail.com>
2023-06-21 21:02:16 -0400

    s

--- isnDivisibleByXandY.js
+++ isnDivisibleByXandY.js
@@ -6,7 +6,7 @@
 // 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
 // 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
 
-function isDivisible(n, x, y) {
+const isDivisible = (n, x, y) => {
     return n%x === 0 && n%y ===0 ? true : false
     
   }
