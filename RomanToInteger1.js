513a7c36e1aecbf964772255aa6c33e288523cbf
Diego Herrera <dagomessis@gmail.com>
2023-09-11 15:48:12 -0400

    Roman to integer refresher

---
+++ RomanToInteger1.js
@@ -0,0 +1,90 @@
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+ 
+
+// Example 1:
+
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+// Example 2:
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+// Example 3:
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 15
+// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+
+
+//Convert roman integers to integers.
+
+//Pseudocode:
+/*
+create a map to map each roman number to integer
+"I":1
+"V":5 
+etc
+->
+create a for loop that runs through the argument, and adds each integer to a variable set to 0
+however we will keep track of each element by having a current variable as well as next variable
+we will check if current < next then we subtract next - current and after the loop is complete we
+return the total result
+*/
+
+
+
+
+const romanToInteger = (value) => {
+let result = 0;    
+let map = new Map();
+
+map.set("I", 1);
+map.set("V", 5);
+map.set("X", 10);
+map.set("L", 50);
+map.set("C", 100);
+map.set("D", 500);
+map.set("M", 1000);
+
+for(let i = 0; i < value.length; i++){
+    let current = map.get(value[i])
+    let next = map.get(value[i + 1])
+    if(current < next){
+        result += next - current;
+        i++;
+    } else {
+        result += current;
+    }
+
+}
+    return result
+
+}
