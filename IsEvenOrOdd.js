3b2648fd1b39b85776651f60a692e521ec7dc6db
Diego Herrera <dagomessis@gmail.com>
2024-01-08 07:14:35 -0500

    is even or odd

---
+++ IsEvenOrOdd.js
@@ -0,0 +1,5 @@
+function evenOrOdd(number) {
+    //if number is even -> return "Even" else return "Odd"
+    //if(number%2 === 0) return "Even" else return "Odd"
+    return number%2 === 0 ? "Even" : "Odd";
+  }
