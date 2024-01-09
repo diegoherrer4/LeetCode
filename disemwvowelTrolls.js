8da795dde40d7b4f466f5c21812509e902e18269
Diego Herrera <dagomessis@gmail.com>
2024-01-09 10:17:23 -0500

    s

--- disemwvowelTrolls.js
+++ disemwvowelTrolls.js
@@ -4,7 +4,7 @@ function disemvowel(str) {
     let vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
     let result = "";
   
-    for (let i of str) {
+    for (let i of str){
       // Check if the current character is in the vowels array
       if (!vowels.includes(i)) {
         // If not, add it to the result string
