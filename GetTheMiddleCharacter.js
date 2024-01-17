e0872b4a1b2b475f670b222c43adf81730843701
Diego Herrera <dagomessis@gmail.com>
2024-01-17 12:46:05 -0500

    s

--- GetTheMiddleCharacter.js
+++ GetTheMiddleCharacter.js
@@ -4,7 +4,7 @@ function getMiddle(s)
   let high = s.length
   let m = Math.floor(low + (high - low) / 2)
   
-  if (high%2 === 0) {
+  if (high % 2 === 0) {
     return `${s[m - 1]}${s[m]}`
   } else {
     return `${s[m]}`
