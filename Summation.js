40922eb76d62568ed7f00c19a1531d7f8e8a1c14
Diego Herrera <dagomessis@gmail.com>
2024-02-01 09:47:05 -0500

    summation

---
+++ Summation.js
@@ -0,0 +1,8 @@
+var summation = function (num) {
+    //Takes in a integer and returns an integer
+  let count = 0;
+    for(let i = 1; i <=num; i++){
+      count += i 
+      }
+  return count;
+}
