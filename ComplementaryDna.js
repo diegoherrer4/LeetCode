6f69624b803f7c3fdd045cd2a241c319d638e327
Diego Herrera <dagomessis@gmail.com>
2024-02-07 08:51:39 -0500

    complementary DNA

---
+++ ComplementaryDna.js
@@ -0,0 +1,13 @@
+function dnaStrand(dna){
+    let compDNA = ''
+    let dnaMap = new Map()
+    dnaMap.set('A', 'T')
+    dnaMap.set('T', 'A')
+    dnaMap.set('C', 'G')
+    dnaMap.set('G', 'C')
+    
+     for(i of dna){  
+      compDNA += dnaMap.get(i)
+      }
+    return compDNA;
+  }
