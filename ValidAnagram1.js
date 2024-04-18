e6c1433aa8501acd74b40af88daf4081aed39abf
Diego Herrera <dagomessis@gmail.com>
2024-04-18 13:23:10 -0400

    valid anagram

---
+++ ValidAnagram1.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let str1 = s.split('').sort().join('')
+    let str2 = t.split('').sort().join('')
+    return str1 == str2
+};
