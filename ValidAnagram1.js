f3452db957f083486bb6113050e2e3c1fa02402e
Diego Herrera <dagomessis@gmail.com>
2024-04-19 20:41:27 -0400

    d

--- ValidAnagram1.js
+++ ValidAnagram1.js
@@ -5,6 +5,6 @@
  */
 var isAnagram = function(s, t) {
     let str1 = s.split('').sort().join('')
-    let str2 = t.split('').sort().join('')
+    let str2 = t.split('').sort().join('');
     return str1 == str2
 };
